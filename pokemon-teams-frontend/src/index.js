b69708b6423917b6425a8c5a92273dc068b319df
brewchetta <chett.tiller@gmail.com>
2020-04-08 14:30:15 -0400

    a couple minor annotations so I feel better

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,9 +1,12 @@
+// endpoint constants
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-// elements
+// element constants
 const main = document.querySelector('main')
+
+
 
 function getTrainers() {
   return fetch(TRAINERS_URL)
